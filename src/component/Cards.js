8d629c470e1aaee008da13721c952a889874ffca
AE/BHH <ehsaniabbas9@gmail.com>
2023-08-07 15:42:31 -0600

    solved react components error in console

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -10,18 +10,16 @@ export default function Cards({ randomWord }) {
 						<Card.Header className='cardHeader'>
 							<h3>{randomWord.word}</h3>
 						</Card.Header>
-						<Card.Title style={{ margin: '20px' }}>
+						<Card.Title style={{ margin: '15px' }}>
 							<h3>{randomWord.phonetic}</h3>
 						</Card.Title>
 						<Card.Body className='cardBody'>
 							{randomWord.meanings.map((item, index) => {
 								if (index <= 0) {
 									return (
-										<Card.Text style={{ margin: '20px' }} key={index}>
-											<div>
-												<h4>Definition: </h4>
-												<h5>{item.definitions[0].definition}</h5>
-											</div>
+										<Card.Text style={{ margin: '15px' }} key={index}>
+											<span className='term'>Definition: </span> <br></br>
+											<span className='definition'>{item.definitions[0].definition}</span>
 										</Card.Text>
 									)
 								}
